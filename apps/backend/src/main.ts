6bf0f32514f6958081088bc7d31c0f78cb948e2a
sakshi <jhasakshi639@gmail.com>
2025-10-17 19:56:06 +0530

    Initial commit

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000',
+    'https://nestjs-project-web.vercel.app'
+  ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
   });
